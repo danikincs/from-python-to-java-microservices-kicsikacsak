9b494cb2f94c192abb7e4fb20000a46c657bc8fe
codecoolmz <vagasiferi4ever@gmail.com>
2017-01-11 14:54:03 +0100

    add second banner

--- src/main/java/banner_service/Service/Service.java
+++ src/main/java/banner_service/Service/Service.java
@@ -8,9 +8,14 @@ import org.json.JSONObject;
 public class Service {
 
     private static Service instance;
-    private static String HTML = "<div class=\"container\">" +
+    private static String basic_banner_HTML = "<div class=\"container\">" +
             "<div class=\"col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3\">" +
             "<a href=\"https://codecool.hu/\"><img src=\"http://www.auplod.com/u/opladu8f6e7.gif\" border=\"1\">" +
+            "</a></div></div>";
+
+    private static String customer_HTML = "<div class=\"container\">" +
+            "<div class=\"col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3\">" +
+            "<a href=\"https://codecool.hu/\"><img src=\"http://www.auplod.com/u/aoludp8f70b.gif\" border=\"1\">" +
             "</a></div></div>";
 
     private Service() {
@@ -26,13 +31,13 @@ public class Service {
     public JSONObject getBanner(){
         JSONObject obj = new JSONObject();
         obj.put("status","done");
-        obj.put("Advertisement", HTML);
+        obj.put("Advertisement", basic_banner_HTML);
         return obj;
     }
 
     public JSONObject getBanner(String user) {
         JSONObject obj = new JSONObject();
-
+        obj.put("Advertisement", customer_HTML);
         obj.put("status", "done");
         return obj;
     }
